5df7e4d3d800d1fdf522ce7d005c9876d9aa13c9
Simeon H.K. Fitch <fitch@datamininglab.com>
2015-05-02 16:30:12 -0400

    Added ability to override location of `settings.xml` file, in part to enable easier testing of extracting global maven settings.

--- src/main/scala/com/typesafe/sbt/pom/Keys.scala
+++ src/main/scala/com/typesafe/sbt/pom/Keys.scala
@@ -5,6 +5,7 @@ import org.apache.maven.model.Model
 
 object SbtPomKeys {
   val pomLocation = SettingKey[File]("mvn-pom-location", "The location where we can find a pom file.")
+  val settingsLocation = SettingKey[File]("mvn-settings-location", "The location of the maven settings.xml file.")
   val profiles = SettingKey[Seq[String]]("mvn-profiles", "List of maven profiles to be applied.")
   val mvnLocalRepository = SettingKey[File]("mvn-local-repository", "The location of the maven local repository we can use to cache artifacts.")
   val effectivePom = SettingKey[Model]("mvn-effective-pom", "Reads the maven effective pom.")
