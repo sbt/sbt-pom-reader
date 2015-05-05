4658cc2758e2c16852bae8c056a2b97830f8f213
Simeon H.K. Fitch <fitch@datamininglab.com>
2015-05-04 20:59:59 -0400

    Renamed plugin and key definition files to match type names.

--- src/main/scala/com/typesafe/sbt/pom/Plugin.scala
+++
@@ -1,8 +0,0 @@
-package com.typesafe.sbt.pom
-
-import sbt._
-
-/** sbt plugin definition for Maven POM reader. */
-object PomReaderPlugin extends Plugin {
-  val useMavenPom: Seq[Setting[_]] = MavenHelper.useMavenPom
-}
