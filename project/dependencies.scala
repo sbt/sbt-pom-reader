83b6253506215223c4f848535f49c1d66a7002ba
Devon Stewart <dstewart@twilio.com>
2018-01-12 01:05:49 -0800

    Removing unnecessary dependencies

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,32 +6,18 @@ object Dependencies {
   val mvnWagonVersion = "2.12"
   val mvnResolver = "1.1.0"
 
-  // val mvnAether           = "org.apache.maven" % "maven-resolver-provider" % mvnVersion
-  // val resolverApi         = "org.apache.maven.resolver" % "maven-resolver-api" % mvnResolver
-  // val resolverConnBasic   = "org.apache.maven.resolver" % "maven-resolver-connector-basic" % mvnResolver
-  // val resolverSpi         = "org.apache.maven.resolver" % "maven-resolver-spi" % mvnResolver
   val transportWagon      = "org.apache.maven.resolver" % "maven-resolver-transport-wagon" % mvnResolver
-  // val transportHttp       = "org.apache.maven.resolver" % "maven-resolver-transport-http" % mvnResolver
   val mvnEmbedder         = "org.apache.maven" % "maven-embedder" % mvnVersion
 
   val mvnWagonLwHttp      = "org.apache.maven.wagon" % "wagon-http-lightweight" % mvnWagonVersion
   val mvnWagonFile        = "org.apache.maven.wagon" % "wagon-file" % mvnWagonVersion
-  // val mvnWagon            = "org.apache.maven.wagon" % "wagon-http" % mvnWagonVersion
-  // val mvnWagonProviderApi = "org.apache.maven.wagon" % "wagon-provider-api" % mvnWagonVersion
 
 
   def pluginDependencies =
      Seq(
-      // mvnAether,
       mvnEmbedder,
-      // mvnWagon,
       mvnWagonFile,
       mvnWagonLwHttp,
-      // mvnWagonProviderApi,
-      // resolverApi,
-      // resolverConnBasic,
-      // resolverSpi,
       transportWagon,
-      // transportHttp,
     )
 }
