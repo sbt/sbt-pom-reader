d29e159c8a9a516a870721e9e81789fc86df3159
Devon Stewart <dstewart@twilio.com>
2018-01-14 10:57:44 -0800

    Manually resolving dependency conflicts in maven-embedder

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -6,12 +6,19 @@ object Dependencies {
   val mvnResolver = "1.1.0"
 
   val transportWagon      = "org.apache.maven.resolver" % "maven-resolver-transport-wagon" % mvnResolver
-  val mvnEmbedder         = "org.apache.maven" % "maven-embedder" % mvnVersion
+  val mvnEmbedder         = "org.apache.maven" % "maven-embedder" % mvnVersion exclude("com.google.guava", "guava") exclude("org.codehaus.plexus", "plexus-utils")
 
+  // These were explicitly added to resolve dependency conflicts in
+  // maven-embedder. Please keep these up-to-date, with the goal of
+  // eventually removing them.
+  val mvnEmbedderDeps = Seq(
+    "com.google.guava" % "guava" % "20.0",
+    "org.codehaus.plexus" % "plexus-utils" % "3.1.0"
+  )
 
   def pluginDependencies =
      Seq(
       mvnEmbedder,
       transportWagon,
-    )
+    ) ++ mvnEmbedderDeps
 }
